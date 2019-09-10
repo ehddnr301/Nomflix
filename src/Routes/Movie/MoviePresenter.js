1ea6e9a2cb2289b962758197a47cd6a7d84d01e2
kor-Don <dy950328@gmail.com>
2019-09-11 00:50:02 +0900

    kmdb Blocked me...

--- src/Routes/Movie/MoviePresenter.js
+++ src/Routes/Movie/MoviePresenter.js
@@ -15,57 +15,60 @@ const MoviePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
   loading ? (
     <Loader></Loader>
   ) : (
-    <Container>
-      <Helmet>
-        <title>EngMovie | Nomflix</title>
-      </Helmet>
-      {nowPlaying && nowPlaying.length > 0 && (
-        <Section title="Now Playing">
-          {nowPlaying.map(movie => (
-            <Poster
-              key={movie.id}
-              id={movie.id}
-              imageUrl={movie.poster_path}
-              title={movie.original_title}
-              rating={movie.vote_average}
-              year={movie.release_date.substring(0, 4)}
-              isMovie={true}
-            />
-          ))}
-        </Section>
-      )}
-      {upcoming && upcoming.length > 0 && (
-        <Section title="Upcoming Movies">
-          {upcoming.map(movie => (
-            <Poster
-              key={movie.id}
-              id={movie.id}
-              imageUrl={movie.poster_path}
-              title={movie.original_title}
-              rating={movie.vote_average}
-              year={movie.release_date.substring(0, 4)}
-              isMovie={true}
-            />
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Movies">
-          {popular.map(movie => (
-            <Poster
-              key={movie.id}
-              id={movie.id}
-              imageUrl={movie.poster_path}
-              title={movie.original_title}
-              rating={movie.vote_average}
-              year={movie.release_date.substring(0, 4)}
-              isMovie={true}
-            />
-          ))}
-        </Section>
-      )}
-      {error && <Message color="#e74c3c" text={error} />}
-    </Container>
+    (alert("I used KMDB but When I use netlify It blocked me... why??"),
+    (
+      <Container>
+        <Helmet>
+          <title>EngMovie | Nomflix</title>
+        </Helmet>
+        {nowPlaying && nowPlaying.length > 0 && (
+          <Section title="Now Playing">
+            {nowPlaying.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {upcoming && upcoming.length > 0 && (
+          <Section title="Upcoming Movies">
+            {upcoming.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Movies">
+            {popular.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#e74c3c" text={error} />}
+      </Container>
+    ))
   );
 
 MoviePresenter.propTypes = {
